9b9f12443717e6f2a443a977568a7662b137fbd4
sowmyavasudeva <vsowmya.05@gmail.com>
2018-03-04 15:19:19 -0800

    Added function to retrieve public ip address

--- src/com/getLocation.java
+++ src/com/getLocation.java
@@ -28,14 +28,14 @@ import java.net.ProtocolException;
 
 public class getLocation 
 {
-	 private static String url = "https://ipfind.co/?ip=104.156.99.192&auth=fdd15277-8fbf-42fe-9778-4ea3281b3a90";
+	 private static String url = null;
 	 
 
 	
 	public String IP2LocationRequest() {
+		String ip = getIPAddress();
+		url = "https://ipfind.co/?ip=" + ip + "&auth=fdd15277-8fbf-42fe-9778-4ea3281b3a90";
 		String jsonStr = makeServiceCall(url);
-		
-		
 		return jsonStr;
 	}
 	
@@ -59,6 +59,27 @@ public class getLocation
         		e.printStackTrace();
         }
         return response;
+		
+	}
+	
+	private String getIPAddress() {
+		String systemipaddress = "";
+        try
+        {
+            URL url_name = new URL("http://bot.whatismyipaddress.com");
+ 
+            BufferedReader sc =
+            new BufferedReader(new InputStreamReader(url_name.openStream()));
+ 
+            // reads system IPAddress
+            systemipaddress = sc.readLine().trim();
+        }
+        catch (Exception e)
+        {
+            systemipaddress = "Cannot Execute Properly";
+        }
+        System.out.println("Public IP Address: " + systemipaddress +"\n");
+        return systemipaddress;
 	}
 	
 	private String convertStreamToString(InputStream is) {
